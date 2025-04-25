035fe498a545bfc13a0519879e21446f48f79c75
Duncan Harvey <duncan.harvey@datadoghq.com>
2025-04-25 10:51:25 -0400

    rename trace-agent crate to datadog-trace-agent

--- crates/datadog-serverless-compat/src/main.rs
+++ crates/datadog-serverless-compat/src/main.rs
@@ -16,7 +16,7 @@ use tokio::{
 };
 use tracing_subscriber::EnvFilter;
 
-use trace_agent::{
+use datadog_trace_agent::{
     aggregator::TraceAggregator,
     config, env_verifier, mini_agent, stats_flusher, stats_processor,
     trace_flusher::{self, TraceFlusher},
