263d694373b44ad52c7a3ee466727dc491ed7367
Duncan Harvey <duncan.harvey@datadoghq.com>
2025-12-03 15:40:13 -0500

    add stats generator and concentrator

---
+++ crates/datadog-trace-agent/src/stats_generator.rs
@@ -0,0 +1,49 @@
+use crate::stats_concentrator_service::{StatsConcentratorHandle, StatsError};
+use libdd_trace_utils::tracer_payload::TracerPayloadCollection;
+use tracing::error;
+
+pub struct StatsGenerator {
+    stats_concentrator: StatsConcentratorHandle,
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum StatsGeneratorError {
+    #[error("Error sending trace stats to the stats concentrator: {0}")]
+    ConcentratorCommandError(StatsError),
+    #[error("Unsupported trace payload version. Failed to send trace stats.")]
+    TracePayloadVersionError,
+}
+
+// Extracts information from traces related to stats and sends it to the stats concentrator
+impl StatsGenerator {
+    #[must_use]
+    pub fn new(stats_concentrator: StatsConcentratorHandle) -> Self {
+        Self { stats_concentrator }
+    }
+
+    pub fn send(&self, traces: &TracerPayloadCollection) -> Result<(), StatsGeneratorError> {
+        if let TracerPayloadCollection::V07(traces) = traces {
+            for trace in traces {
+                // Set tracer metadata
+                if let Err(err) = self.stats_concentrator.set_tracer_metadata(trace) {
+                    error!("Failed to set tracer metadata: {err}");
+                    return Err(StatsGeneratorError::ConcentratorCommandError(err));
+                }
+
+                // Generate stats for each span in the trace
+                for chunk in &trace.chunks {
+                    for span in &chunk.spans {
+                        if let Err(err) = self.stats_concentrator.add(span) {
+                            error!("Failed to send trace stats: {err}");
+                            return Err(StatsGeneratorError::ConcentratorCommandError(err));
+                        }
+                    }
+                }
+            }
+            Ok(())
+        } else {
+            error!("Unsupported trace payload version. Failed to send trace stats.");
+            Err(StatsGeneratorError::TracePayloadVersionError)
+        }
+    }
+}
