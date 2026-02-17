1e4569bc20c599a3c825990a7eb69ee94875719e
Jordan Gonzalez <30836115+duncanista@users.noreply.github.com>
2026-02-17 11:58:14 -0500

    add integration test

--- crates/datadog-serverless-compat/src/main.rs
+++ crates/datadog-serverless-compat/src/main.rs
@@ -242,6 +242,7 @@ async fn start_dogstatsd(
         port,
         metric_namespace,
         so_rcvbuf: None,
+        buffer_size: None,
     };
     let dogstatsd_cancel_token = tokio_util::sync::CancellationToken::new();
 
