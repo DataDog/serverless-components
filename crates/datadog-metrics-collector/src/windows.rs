41635f853ed30c1c112d04818908c369a5f8b3cf
Kathie Huang <kathie.huang@datadoghq.com>
2026-03-30 13:24:01 -0400

    Turn off DD_ENHANCED_METRICS in Windows for now to prevent metrics collector from initializing and logging that metrics collection is being skipped

--- crates/datadog-metrics-collector/src/windows.rs
+++ crates/datadog-metrics-collector/src/windows.rs
@@ -9,13 +9,11 @@
 //! All CPU metrics are reported in nanocores (1 core = 1,000,000,000 nanocores).
 
 use crate::cpu::{CpuStats, CpuStatsReader};
-use tracing::debug;
 
 pub struct WindowsCpuStatsReader;
 
 impl CpuStatsReader for WindowsCpuStatsReader {
     fn read(&self) -> Option<CpuStats> {
-        debug!("CPU enhanced metrics are not yet supported on Windows Azure Functions");
         None
     }
 }
